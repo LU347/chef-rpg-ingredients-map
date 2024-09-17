6e4933a7d60216c45849ab7d5dcb6dfb2c6e6037
LU347 <103535661+LU347@users.noreply.github.com>
2024-09-17 13:17:54 -0500

    Temporarily removed sidebar

--- chef-rpg-ingredients-map/src/App.jsx
+++ chef-rpg-ingredients-map/src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import Sidebar from './sidebar.jsx';
 import Map from './Map.jsx';
 import gameMap from './assets/chef-rpg-map.webp'
 import './App.css'
@@ -9,7 +7,6 @@ function App() {
     <>
       <img className="chef-rpg-map" src={gameMap} alt="Map of ChefRPG"></img>
       <Map />
-      <Sidebar />
     </>
   )
 }
