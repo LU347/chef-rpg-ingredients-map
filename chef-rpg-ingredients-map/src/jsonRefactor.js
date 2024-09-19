0f36ef91233336af97a03244277c5c4e9f702d2b
LU347 <103535661+LU347@users.noreply.github.com>
2024-09-19 16:40:45 -0500

    Refactored JSON
    Also updated how item data is retrieved

--- chef-rpg-ingredients-map/src/jsonRefactor.js
+++
@@ -1,5 +0,0 @@
-//TODO: parse coordinates.json and check if it doesn't have an id
-// if an object doesn't have an id:
-//  find id associated with the label
-//  add id key w/ value
-// delete label and type keys
