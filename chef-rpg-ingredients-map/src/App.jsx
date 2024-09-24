06a1bf03a8d25293961dfbf949905dc42a5e64c1
LU347 <103535661+LU347@users.noreply.github.com>
2024-09-23 22:28:54 -0500

    Sidebar front end

--- chef-rpg-ingredients-map/src/App.jsx
+++ chef-rpg-ingredients-map/src/App.jsx
@@ -1,9 +1,11 @@
 import Map from './Map.jsx';
 import './App.css'
+import Sidebar from './sidebar.jsx';
 
 function App() {
   return (
     <div id="app">
+      <Sidebar />
       <Map />
     </div>
   )
--- chef-rpg-ingredients-map/src/Sidebar.jsx
+++ chef-rpg-ingredients-map/src/Sidebar.jsx
@@ -1,25 +1,82 @@
 import React, { useState } from "react";
+import { useRef } from "react";
+
 import sidebarOpenIcon from './assets/chevron_backward.svg';
 import sidebarClosedIcon from './assets/chevron_forward.svg';
 import "./Sidebar.css";
 
+import itemTypes from './lib/itemTypes.json';
+import itemData from './lib/itemData.json';
+
+var sidebar = document.getElementById("sidebar");
+
 const Sidebar = () => {
-    const [isOpen, toggleSidebar] = useState(false);
+    const [isOpen, toggleSidebar] = useState(true);
+
+    const ref = useRef();
 
     const handleToggle = () => {
-        toggleSidebar(!isOpen)
+        if (ref.current.style.right === "0%") {
+            ref.current.style.right = "-15%";
+        } else {
+            ref.current.style.right = "0%";
+        }
     };
 
+
     return (
-        <div className="sidebar">
-            {!isOpen ? (
-                <button onClick={handleToggle}><img src={sidebarOpenIcon} alt="Open Sidebar"></img></button>
-            ) : (
-                <div>
-                    <button onClick={handleToggle}><img src={sidebarClosedIcon} alt="Close Sidebar"></img></button> 
-                    <p>Sidebar Content</p> 
+        <div ref={ref} id="sidebar" className="sidebar">
+            <button id="sidebarButton" onClick={handleToggle}><img src={sidebarClosedIcon} alt="Close Sidebar"></img></button>
+            <div className="menu">
+                <h2>Season</h2>
+                <select>
+                    <option>Spring</option>
+                    <option>Summer</option>
+                    <option>Fall</option>
+                    <option>Winter</option>
+                </select>
+                <hr></hr>
+                <input type="text" placeholder="Search Ingredient"></input>
+                <h3>Types</h3>
+                <div className="types-container">
+                    {itemTypes.map(item => (
+                        <div key={item.name}>
+                            <input type="checkbox" id={item.name} value={item.name}></input>
+                            <label htmlFor={item.name}>{item.name.charAt(0).toUpperCase() + item.name.slice(1)}</label>
+                        </div>
+                    ))}
                 </div>
-            )}
+                <h3>Ingredients</h3>
+                <button>Hide all</button>
+                <div className="ingredients-container">
+                    {itemData.map(item => (
+                        item.type !== "building" ?
+                            (
+                                <div key={item.id}>
+                                    <input type="checkbox" id={item.name} value={item.name}></input>
+                                    <label htmlFor={item.name}>{item.name}</label>
+                                </div>
+                            ) : null
+                    ))}
+                </div>
+                <h3>Buildings</h3>
+                <div className="buildings-container">
+                    {itemData.map(item => (
+                        item.type === "building" ?
+                            (
+                                <div key={item.id}>
+                                    <input type="checkbox" id={item.name} value={item.name}></input>
+                                    <label htmlFor={item.name}>{item.name}</label>
+                                </div>
+                            ) : null
+                    ))}
+                </div>
+                <h3>NPCs</h3>
+                <h3>Misc</h3>
+                <div>
+                    <button>Toggle Labels</button>
+                </div>
+            </div>
         </div>
     );
 }
